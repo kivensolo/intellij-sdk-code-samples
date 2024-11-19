c2fe08d9b90d2c1eb02707ebcdc96fc7d1b5f9b8
zhi.wang <zhi.wang@starcor.com>
2024-11-19 14:52:15 +0800

    Title(build)：更新构建系统的配置，加入config配置文件 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 额外说明：N/A

--- zeke_wildfire_samples/build.gradle.kts
+++ zeke_wildfire_samples/build.gradle.kts
@@ -1,53 +1,91 @@
+apply {
+    from("config.gradle.kts")
+}
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.8.21"
     id("org.jetbrains.intellij") version "1.13.3"
 }
 
-group = "com.wildfire"
-version = "1.0-SNAPSHOT"
+val ideVersion: String by extra
+val baseVersion: String by extra
+val javaVersion: String by extra
+val since: String by extra
+val until: String? by extra
+val mPluginName: String by extra
+val mPluginId: String by extra
+val mPluginVersion: String by extra
+
 
 repositories {
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/google")
     mavenCentral()
 }
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2020.2.3")
+    version.set("2022.3.3")
     type.set("IC") // Target IDE Platform
 
     //依赖IDEA平台的java插件
-    plugins.set(listOf("java"))
+    plugins.set(listOf("com.intellij.java"))
+}
+
+java {
+    sourceSets.main {
+        this.java.srcDir("src/main/java")
+    }
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    }
+
 }
 
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+        options.encoding = "UTF-8"
     }
 
     compileKotlin {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions{
+            jvmTarget = javaVersion
+        }
     }
 
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
+    patchPluginXml{
+        pluginId.set(mPluginId)
+        sinceBuild.set(since)
+        if(until.isNullOrEmpty()){
+            untilBuild.set(provider {  null })
+        } else {
+            untilBuild.set(until)
+        }
+        version.set(mPluginVersion)
+        changeNotes.set("""
+          <ul>
+            <li>v1.0 Demo Init.</li>
+          </ul>
+          """)
     }
 
-    patchPluginXml {
-        sinceBuild.set("202")
-        untilBuild.set("232.*")
+    jar {
+        archiveFileName.set("wildfire-demo.jar")
     }
 
-//    signPlugin {
-//        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-//        privateKey.set(System.getenv("PRIVATE_KEY"))
-//        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-//    }
-//
-//    publishPlugin {
-//        token.set(System.getenv("PUBLISH_TOKEN"))
-//    }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
---
+++ zeke_wildfire_samples/config.gradle.kts
@@ -0,0 +1,12 @@
+extra["ideVersion"] = "IC-2022.3.3"
+extra["baseVersion"] = "232.8660.185"
+extra["ideaPluginVersion"] = "1.13.3"
+extra["kotlinVersion"] = "1.8.21"
+extra["javaVersion"] = "17"
+
+extra["mPluginName"] = "NeckProtector"
+extra["mPluginId"] = "com.wildfire"
+extra["mPluginVersion"] = "v1.0.0"
+
+extra["since"] = "202"
+extra["until"] = "232.*"
