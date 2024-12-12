d56e6cb9e10bf0660e29cb3e23e246a1ebf14922
zhi.wang <zhi.wang@starcor.com>
2024-12-12 09:23:15 +0800

    Title(refactor)：将FakePsiElementReference放到ZekeProtocolReferenceProvider类中。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 额外说明：N/A

--- zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/ZekeProtocolReferenceProvider.kt
+++ zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/ZekeProtocolReferenceProvider.kt
@@ -1,17 +1,17 @@
 package org.intellij.samples.psi.reference
 
+import com.intellij.diagnostic.LoadingState
+import com.intellij.ide.browsers.*
 import com.intellij.json.psi.JsonProperty
 import com.intellij.json.psi.JsonStringLiteral
 import com.intellij.json.psi.JsonValue
 import com.intellij.openapi.util.TextRange
-import com.intellij.psi.ElementManipulators
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiReference
-import com.intellij.psi.PsiReferenceProvider
+import com.intellij.psi.*
+import com.intellij.psi.impl.FakePsiElement
 import com.intellij.util.ProcessingContext
 
 /**
- * 创建引用提供者
+ * 一个zeke://协议的引用提供，实现点击跳转打开浏览器。
  */
 class ZekeProtocolReferenceProvider: PsiReferenceProvider() {
 
@@ -39,10 +39,86 @@ class ZekeProtocolReferenceProvider: PsiReferenceProvider() {
 //            val valueTextRange = TextRange.create("zeke://".length +  1,textValue.length + 1)
 //            val valueTextRange = TextRange.from(1, textValue.length + 1)
             //TODO 这是在计算什么范围？
-            val valueTextRange:TextRange = ElementManipulators.getValueTextRange(element);
+            val valueTextRange:TextRange = ElementManipulators.getValueTextRange(element)
             if (valueTextRange.isEmpty) return PsiReference.EMPTY_ARRAY
             return arrayOf(FakePsiElementReference(element,valueTextRange))
         }
         return PsiReference.EMPTY_ARRAY
     }
 }
+
+
+/**
+ * 一个虚假的PSIElement
+ */
+class FakePsiElementReference(
+    private val element: PsiElement,
+    private var textRange: TextRange?
+): PsiReferenceBase<PsiElement>(element,textRange) {
+    private var urlContent: String = ""
+
+    init {
+        textRange?.let {
+            urlContent = element.text.substring(it.startOffset, it.endOffset)
+        }
+    }
+
+    override fun resolve(): PsiElement {
+        return OpenBrowseFakePsiElement()
+    }
+
+    override fun getCanonicalText(): String {
+        return element.text
+    }
+
+    override fun getRangeInElement(): TextRange {
+        if(textRange == null){
+            textRange = calculateDefaultRangeInElement();
+        }
+        return textRange!!
+    }
+
+    fun getUrl(): String {
+        return urlContent
+    }
+
+    /**
+     * 一个假的PSIElement，导航跳转到浏览器
+     */
+    inner class OpenBrowseFakePsiElement : FakePsiElement(), SyntheticElement {
+        override fun getParent(): PsiElement {
+            return myElement
+        }
+
+        override fun navigate(requestFocus: Boolean) {
+            var effectiveBrowser: WebBrowser? = null
+            val browserManager = WebBrowserManager.getInstance()
+            // 默认的浏览器配置策略，通过seeting/tools/web browser and preview 来设置
+            if (browserManager.defaultBrowserPolicy == DefaultBrowserPolicy.FIRST) {
+                effectiveBrowser = browserManager.firstActiveBrowser
+            }
+            getBrowserLauncher().browse(getUrl(), browser = effectiveBrowser)
+        }
+
+        private fun getBrowserLauncher(): BrowserLauncher {
+            return if (LoadingState.COMPONENTS_LOADED.isOccurred) BrowserLauncher.instance else BrowserLauncherAppless()
+        }
+
+        override fun getPresentableText(): String {
+            return getUrl()
+        }
+
+        override fun getName(): String {
+            return getUrl()
+        }
+
+        override fun getTextRange(): TextRange {
+            val rangeInElement: TextRange = rangeInElement
+            val elementRange: TextRange = myElement.textRange
+            return rangeInElement.shiftRight(elementRange.startOffset)
+        }
+    }
+
+
+}
+
