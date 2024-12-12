edabd268f021ea07ab234cdd69c11683678a08d0
zhi.wang <zhi.wang@starcor.com>
2024-12-12 09:26:11 +0800

    Title(refactor)：重新调整FakePsiElementReference，将FakePsiElementReference类放到ZekeProtocolReferenceProvider类中。 Cause：N/A Solution：N/A Effect: N/A Reviewer: N/A 额外说明：N/A

--- zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/ZekeProtocolReferenceProvider.kt
+++ zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/ZekeProtocolReferenceProvider.kt
@@ -45,80 +45,80 @@ class ZekeProtocolReferenceProvider: PsiReferenceProvider() {
         }
         return PsiReference.EMPTY_ARRAY
     }
-}
-
-
-/**
- * 一个虚假的PSIElement
- */
-class FakePsiElementReference(
-    private val element: PsiElement,
-    private var textRange: TextRange?
-): PsiReferenceBase<PsiElement>(element,textRange) {
-    private var urlContent: String = ""
-
-    init {
-        textRange?.let {
-            urlContent = element.text.substring(it.startOffset, it.endOffset)
-        }
-    }
-
-    override fun resolve(): PsiElement {
-        return OpenBrowseFakePsiElement()
-    }
-
-    override fun getCanonicalText(): String {
-        return element.text
-    }
-
-    override fun getRangeInElement(): TextRange {
-        if(textRange == null){
-            textRange = calculateDefaultRangeInElement();
-        }
-        return textRange!!
-    }
-
-    fun getUrl(): String {
-        return urlContent
-    }
 
     /**
-     * 一个假的PSIElement，导航跳转到浏览器
+     * 一个虚假的PSIElement
      */
-    inner class OpenBrowseFakePsiElement : FakePsiElement(), SyntheticElement {
-        override fun getParent(): PsiElement {
-            return myElement
-        }
+    class FakePsiElementReference(
+        private val element: PsiElement,
+        private var textRange: TextRange?
+    ): PsiReferenceBase<PsiElement>(element,textRange) {
+        private var urlContent: String = ""
 
-        override fun navigate(requestFocus: Boolean) {
-            var effectiveBrowser: WebBrowser? = null
-            val browserManager = WebBrowserManager.getInstance()
-            // 默认的浏览器配置策略，通过seeting/tools/web browser and preview 来设置
-            if (browserManager.defaultBrowserPolicy == DefaultBrowserPolicy.FIRST) {
-                effectiveBrowser = browserManager.firstActiveBrowser
+        init {
+            textRange?.let {
+                urlContent = element.text.substring(it.startOffset, it.endOffset)
             }
-            getBrowserLauncher().browse(getUrl(), browser = effectiveBrowser)
         }
 
-        private fun getBrowserLauncher(): BrowserLauncher {
-            return if (LoadingState.COMPONENTS_LOADED.isOccurred) BrowserLauncher.instance else BrowserLauncherAppless()
+        override fun resolve(): PsiElement {
+            return OpenBrowseFakePsiElement()
         }
 
-        override fun getPresentableText(): String {
-            return getUrl()
+        override fun getCanonicalText(): String {
+            return element.text
         }
 
-        override fun getName(): String {
-            return getUrl()
+        override fun getRangeInElement(): TextRange {
+            if(textRange == null){
+                textRange = calculateDefaultRangeInElement();
+            }
+            return textRange!!
         }
 
-        override fun getTextRange(): TextRange {
-            val rangeInElement: TextRange = rangeInElement
-            val elementRange: TextRange = myElement.textRange
-            return rangeInElement.shiftRight(elementRange.startOffset)
+        fun getUrl(): String {
+            return urlContent
         }
+
+        /**
+         * 一个假的PSIElement，导航跳转到浏览器
+         */
+        inner class OpenBrowseFakePsiElement : FakePsiElement(), SyntheticElement {
+            override fun getParent(): PsiElement {
+                return myElement
+            }
+
+            override fun navigate(requestFocus: Boolean) {
+                var effectiveBrowser: WebBrowser? = null
+                val browserManager = WebBrowserManager.getInstance()
+                // 默认的浏览器配置策略，通过seeting/tools/web browser and preview 来设置
+                if (browserManager.defaultBrowserPolicy == DefaultBrowserPolicy.FIRST) {
+                    effectiveBrowser = browserManager.firstActiveBrowser
+                }
+                getBrowserLauncher().browse(getUrl(), browser = effectiveBrowser)
+            }
+
+            private fun getBrowserLauncher(): BrowserLauncher {
+                return if (LoadingState.COMPONENTS_LOADED.isOccurred) BrowserLauncher.instance else BrowserLauncherAppless()
+            }
+
+            override fun getPresentableText(): String {
+                return getUrl()
+            }
+
+            override fun getName(): String {
+                return getUrl()
+            }
+
+            override fun getTextRange(): TextRange {
+                val rangeInElement: TextRange = rangeInElement
+                val elementRange: TextRange = myElement.textRange
+                return rangeInElement.shiftRight(elementRange.startOffset)
+            }
+        }
+
+
     }
-
-
 }
+
 
