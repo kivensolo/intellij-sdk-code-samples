c7cb0243ce63a327f31dd362160914594fc0c836
zhi.wang <zhi.wang@starcor.com>
2024-12-17 16:11:20 +0800

    Title(optimization)：优化AndroidAssetsURIReference的相关代码 Cause：删除multiResolve中已经无用的判断逻辑代码。 Solution：N/A Effect: N/A Reviewer: N/A 额外说明：N/A

--- zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/AndroidAssetsURIReference.kt
+++ zeke_wildfire_samples/src/main/kotlin/org/intellij/samples/psi/reference/AndroidAssetsURIReference.kt
@@ -1,21 +1,14 @@
 package org.intellij.samples.psi.reference
 
-import com.android.tools.idea.projectsystem.sourceProviders
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
 import org.intellij.utils.findFilesInAssets
-import org.intellij.utils.getAssetsDirPSIResolve
 import org.jetbrains.android.facet.AndroidFacet
-import org.jetbrains.kotlin.idea.core.util.toPsiDirectory
-import org.jetbrains.kotlin.idea.editor.fixers.end
-import org.jetbrains.kotlin.idea.editor.fixers.start
 
 /**
  * 集成PsiReferenceBase，用于自定义实现的一个Android资源的 URI 引用解析器。
- * 比如可以导航至“file:///”开头的
+ * 比如可以导航至“file:///”开头的assets资源文件
  * 同时继承 PsiPolyVariantReference 接口，实现多解析。
- *
- * ElementManipulators.getValueTextRange(element)
  */
 class AndroidAssetsURIReference(
     psiElement: PsiElement,
@@ -38,7 +31,6 @@ class AndroidAssetsURIReference(
 
     /**
      * 进行多解析判断，返回多个结果，结果数据为PsiElementResolveResult类型保存在数组中。
-     *
      */
     override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> {
         if (incompleteCode) {
@@ -46,11 +38,6 @@ class AndroidAssetsURIReference(
         }
         val androidFacet = AndroidFacet.getInstance(element) ?: return ResolveResult.EMPTY_ARRAY
         val rangeInElement = rangeInElement
-
-        //FIXME 为什么有这个逻辑？ 已经在AndroidAssetsURIReferenceProvider中判断了 uriString.startsWith(prefix)
-        if (rangeInElement.endOffset <  prifixRange.startOffset) {
-            return androidFacet.getAssetsDirPSIResolve().toTypedArray()
-        }
         val elementFullText = myElement.text
         val relativePath = elementFullText.substring( prifixRange.endOffset, rangeInElement.endOffset)
         val psiFilesInAssets = androidFacet.findFilesInAssets(relativePath)
